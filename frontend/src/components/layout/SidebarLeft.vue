4ba65983877cd56a0ea6cdee1b2516ae671cdaaa
Alex <a.swetlow@gmail.com>
2021-02-16 18:30:14 +0100

    :sparkles: Add loading indicator

--- frontend/src/components/layout/SidebarLeft.vue
+++ frontend/src/components/layout/SidebarLeft.vue
@@ -172,12 +172,34 @@
                         </p>
                         <div class="ml-2 flex-shrink-0 flex">
                           <p
+                            v-if="loadingConversation !== conversation.userId"
                             class=" inline-flex text-xs leading-5 text-gray-400 group-hover:text-gray-500 group-focus:text-gray-600"
                             :title="lastConversationItemDate(conversation, false)"
                             :class="[isSelected(conversation) ? 'text-gray-600' : '']"
                           >
-                            {{ lastConversationItemDate(conversation) }}
+                            <span>{{ lastConversationItemDate(conversation) }}</span>
                           </p>
+                          <svg
+                            v-if="loadingConversation === conversation.userId"
+                            class="animate-spin  h-5 w-5 text-jovo-blue self-center"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                          >
+                            <circle
+                              class="opacity-25"
+                              cx="12"
+                              cy="12"
+                              r="10"
+                              stroke="currentColor"
+                              stroke-width="4"
+                            ></circle>
+                            <path
+                              class="opacity-75"
+                              fill="currentColor"
+                              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                            ></path>
+                          </svg>
                         </div>
                       </div>
                       <div class="mt-1q sm:flex sm:justify-between">
@@ -218,6 +240,7 @@ import { mixins } from 'vue-class-component';
 export default class SidebarLeft extends mixins(BaseMixin) {
   search = '';
   openAppPopover = false;
+  loadingConversation: string | undefined = '';
 
   async mounted() {
     try {
@@ -245,6 +268,7 @@ export default class SidebarLeft extends mixins(BaseMixin) {
   }
 
   async selectConversation(inboxLog?: InboxLog) {
+    this.loadingConversation = inboxLog?.userId;
     if (inboxLog) {
       await this.$store.dispatch('DataModule/fetchUserConversations', {
         userId: inboxLog.userId,
@@ -258,6 +282,7 @@ export default class SidebarLeft extends mixins(BaseMixin) {
         });
       }
     }
+    this.loadingConversation = undefined;
   }
 
   lastConversationItemDate(inboxLog: InboxLog, simple = true) {
