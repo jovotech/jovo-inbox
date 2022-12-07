c057629a1a2ae29e3b1d144aede9aa15678f18bf
Alex <a.swetlow@gmail.com>
2022-12-07 14:46:58 +0100

    :wrench: Update ormconfig.js

--- api/ormconfig.js
+++ api/ormconfig.js
@@ -1,18 +1,23 @@
-// eslint-disable-next-line no-undef
+const host = process.env.MYSQL_HOST || 'localhost';
+const username = process.env.MYSQL_USER || 'root';
+const password = process.env.MYSQL_PASSWORD || 'root';
+const database = process.env.MYSQL_DATABASE || 'jovoinbox';
+const port = process.env.MYSQL_PORT || 3306;
+
 module.exports = {
   type: 'mysql',
   synchronize: true,
   logging: false,
   logger: 'simple-console',
-  // eslint-disable-next-line no-undef
-  host: process.env.MYSQL_HOST,
-  port: 3306,
-  // eslint-disable-next-line no-undef
-  username: process.env.MYSQL_USER,
-  // eslint-disable-next-line no-undef
-  password: process.env.MYSQL_PASSWORD,
-  // eslint-disable-next-line no-undef
-  database: process.env.MYSQL_DATABASE,
+  host,
+  port,
+  username,
+  password,
+  database,
   entities: ['dist/**/*.entity{.ts,.js}'],
   charset: 'utf8mb4_unicode_ci',
+  migrations: ['dist/migrations/*.js'],
+  cli: {
+    migrationsDir: 'src/migrations',
+  },
 };
